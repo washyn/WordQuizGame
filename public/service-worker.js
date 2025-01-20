96dee4134ecc5b4449f64adfb1d56ee0a58b6ee5
Washington Acero M <shin321654987@gmail.com>
2025-01-20 10:25:16 -0800

    aded service worker

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "pwa-cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+// Elimina cachés antiguos durante la activación
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta las solicitudes y sirve desde la caché si está disponible
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((fetchResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        // Manejar fallos, como servir un archivo estático de respaldo
+        if (event.request.destination === "document") {
+          return caches.match("/");
+        }
+      })
+  );
+});
