3004cdc15569a60bc321c9cc9ed74f6e404eaebf
Washington Acero M <shin321654987@gmail.com>
2025-01-19 19:07:13 -0800

    aded migration to asp net proyect

--- wordquizgame.client/src/app/app.module.ts
+++ wordquizgame.client/src/app/app.module.ts
@@ -1,19 +1,40 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import Aura from '@primeng/themes/aura';
+import {GetDataStrategy, GetLocalDataStrategy} from "./shared/get-data-strategy";
+import {PrimeSharedModule} from "./shared/prime-shared.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+  imports: [BrowserModule, CommonModule, AppRoutingModule, PrimeSharedModule],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    {
+      provide: MessageService,
+      useClass: MessageService,
+    },
+    {
+      provide: ConfirmationService,
+      useClass: ConfirmationService,
+    },
+    {
+      provide: GetDataStrategy,
+      useClass: GetLocalDataStrategy,
+    },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
