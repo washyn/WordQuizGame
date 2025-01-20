323be79dfc5179b5bc006b3e47831732ca1bfb60
Washington Acero M <shin321654987@gmail.com>
2025-01-20 07:13:22 -0800

    aded changes asp net core

---
+++ WordQuizGame.Server/Controllers/QuestionsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.DependencyInjection;
+
+namespace WordQuizGame.Server.Controllers
+{
+    [ApiController]
+    [Route("api/questions")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly ILogger<QuestionsController> _logger;
+        private readonly SampleLog _sampleLog;
+
+        public QuestionsController(ILogger<QuestionsController> logger, SampleLog sampleLog)
+        {
+            _logger = logger;
+            _sampleLog = sampleLog;
+        }
+
+        [HttpGet()]
+        public void Get()
+        {
+            _logger.LogInformation("Get questions");
+            _sampleLog.Get();
+        }
+    }
+
+    public class SampleLog : ITransientDependency
+    {
+        private readonly ILogger<SampleLog> _logger;
+
+        SampleLog(ILogger<SampleLog> logger)
+        {
+            _logger = logger;
+            _logger = logger;
+        }
+        public void Get()
+        {
+            _logger.LogInformation("Get questions");
+        }
+    }
+}
