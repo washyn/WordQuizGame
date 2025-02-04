dcfe2de1366f65f1198e41fc35d77cfa94944951
Washington Acero M <shin321654987@gmail.com>
2025-02-04 08:47:01 -0800

    aded fixes

--- wordquizgame.client/src/app/shared/get-data-strategy.ts
+++ wordquizgame.client/src/app/shared/get-data-strategy.ts
@@ -1,5 +1,4 @@
-import { ElementWord } from '../app.component';
-import { data } from './data';
+import { ElementWord } from './interfaces';
 
 export interface DataStructure {
   id: string;
@@ -14,64 +13,4 @@ export interface IGetDataStrategy {
 
 export abstract class GetDataStrategy implements IGetDataStrategy {
   abstract getData(): Promise<ElementWord>;
-}
-
-export class GetLocalDataStrategy implements IGetDataStrategy {
-  getData(): Promise<ElementWord> {
-    let resultData: ElementWord = {
-      elementsA: [],
-      elementsB: [],
-    };
-
-    let tempData = this.genData();
-    // map
-    tempData.forEach((element) => {
-      resultData.elementsA.push({
-        id: this.guidGenerator(),
-        text: element[1],
-        idAssociated: element[0],
-      });
-      resultData.elementsB.push({
-        id: this.guidGenerator(),
-        text: element[2],
-        idAssociated: element[0],
-      });
-    });
-
-    return Promise.resolve(resultData);
-  }
-
-  genData() {
-    let nWords = this.gerRandomBetween(10, 10);
-    let counter = 0;
-    let data: [string, string, string][] = [];
-    while (data.length <= nWords) {
-      let randomWord = this.getRandomWord();
-      if (!data.some((element) => element[0] === randomWord[0])) {
-        data.push(randomWord);
-        counter++;
-      }
-    }
-    return data;
-  }
-
-  gerRandomBetween(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  getRandomWord() {
-    let randomIndex = this.gerRandomBetween(0, data.length - 1);
-    return data[randomIndex];
-  }
-
-  guidGenerator() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-      /[xy]/g,
-      function (c) {
-        var r = (Math.random() * 16) | 0,
-          v = c == 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-      }
-    );
-  }
 }
--- wordquizgame.client/src/app/shared/utils.ts
+++ wordquizgame.client/src/app/shared/utils.ts
@@ -7,6 +7,7 @@ export function guidGenerator() {
     return v.toString(16);
   });
 }
+// TODO: improve rangom number generator
 export function gerRandomBetween(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
